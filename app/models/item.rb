f96eafd9e11a5da478bf93e4a2c6d5f96694d6a3
t-masa0724mi <s100.iizuka.masami@gmail.com>
2021-06-02 11:37:15 +0900

    商品出品機能

---
+++ app/models/item.rb
@@ -0,0 +1,29 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :day_to_ship
+  has_one    :purchase
+  has_one_attached :image
+  
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price, format: {with: /\A[0-9]+\z/}
+    validates_inclusion_of :price, in:300..9999999
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_charge_id
+      validates :shipping_area_id
+      validates :day_to_ship_id
+    end
+  end  
+end  
+
+
