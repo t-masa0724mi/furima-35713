92a2f9a8bfc121f15bea753ad84c64c9b2d5e913
t-masa0724mi <s100.iizuka.masami@gmail.com>
2021-06-03 19:01:19 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :ensure_user, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -19,24 +21,30 @@ class ItemsController < ApplicationController
   end  
 
   def show
-    @item = Item.find(params[:id])
   end  
 
   def edit
-    @item = Item.find(params[:id])
   end  
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
     end    
-  end  
+  end 
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  def ensure_user
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end  
+  end    
 end
